dcbe7d82125d7e9cfb84fdbd46c26d194f2da199
Celery Liu <liuct94@gmail.com>
2018-05-26 14:11:56 +0800

    Init

---
+++ LeetcodeAlg.Test/N001TwoSum.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetcodeAlg.Test
+{
+    [TestClass]
+    public class N001TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            System.Collections.Hashtable hash = new System.Collections.Hashtable();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hash.Contains(target - nums[i]))
+                {
+                    return new int[] { (int)hash[target - nums[i]], i };
+                }
+                if (!hash.Contains(nums[i]))
+                    hash.Add(nums[i], i);
+            }
+
+            return new int[] { };
+        }
+
+        [TestMethod]
+        public void Case_001()
+        {
+            var nums = new int[] { 2, 7, 11, 15 };
+            var result = TwoSum(nums, 9);
+
+            Assert.AreEqual(0, result[0]);
+            Assert.AreEqual(1, result[1]);
+        }
+    }
+}
