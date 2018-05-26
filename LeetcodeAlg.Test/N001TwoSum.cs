8f0aee793539a98123bd5beebae82c993157e53a
Celery Liu <liuct94@gmail.com>
2018-05-26 14:34:17 +0800

    Two Sum

--- LeetcodeAlg.Test/N001TwoSum.cs
+++ LeetcodeAlg.Test/N001TwoSum.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LeetcodeAlg.Test
@@ -7,15 +8,15 @@ namespace LeetcodeAlg.Test
     {
         public int[] TwoSum(int[] nums, int target)
         {
-            System.Collections.Hashtable hash = new System.Collections.Hashtable();
-            for (int i = 0; i < nums.Length; i++)
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
             {
-                if (hash.Contains(target - nums[i]))
-                {
-                    return new int[] { (int)hash[target - nums[i]], i };
-                }
-                if (!hash.Contains(nums[i]))
-                    hash.Add(nums[i], i);
+                var result = target - nums[i];
+                if (dictionary.ContainsKey(result)) return new int[] {dictionary[result], i};
+
+                if (!dictionary.ContainsKey(nums[i]))
+                    dictionary.Add(nums[i], i);
             }
 
             return new int[] { };
@@ -24,11 +25,21 @@ namespace LeetcodeAlg.Test
         [TestMethod]
         public void Case_001()
         {
-            var nums = new int[] { 2, 7, 11, 15 };
+            var nums = new int[] {2, 7, 11, 15};
             var result = TwoSum(nums, 9);
 
             Assert.AreEqual(0, result[0]);
             Assert.AreEqual(1, result[1]);
+        }
+
+        [TestMethod]
+        public void Case_002()
+        {
+            var nums = new int[] {2, 2, 7, 11, 15};
+            var result = TwoSum(nums, 9);
+
+            Assert.AreEqual(0, result[0]);
+            Assert.AreEqual(2, result[1]);
         }
     }
 }
